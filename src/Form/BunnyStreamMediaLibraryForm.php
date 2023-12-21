92ea618c5a8fd1f005fdaad2cfe994f87c7eb8e7
Borja Vicente <nireneko@gmail.com>
2023-12-21 10:21:54 +0100

    Added validation for media creation.

--- src/Form/BunnyStreamMediaLibraryForm.php
+++ src/Form/BunnyStreamMediaLibraryForm.php
@@ -2,16 +2,53 @@
 
 namespace Drupal\bunny_stream\Form;
 
+use Drupal\bunny_stream\BunnyStreamManagerFactoryInterface;
 use Drupal\bunny_stream\BunnyStreamSourceInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBuilderInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
 use Drupal\media_library\Form\AddFormBase;
+use Drupal\media_library\MediaLibraryUiBuilder;
+use Drupal\media_library\OpenerResolverInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Creates a form to create media entities from Bunny stream ID's.
  */
 class BunnyStreamMediaLibraryForm extends AddFormBase {
+
+  /**
+   * Constructs an AddFormBase object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\media_library\MediaLibraryUiBuilder $library_ui_builder
+   *   The media library UI builder.
+   * @param \Drupal\media_library\OpenerResolverInterface $opener_resolver
+   *   The opener resolver.
+   */
+  public function __construct(
+    EntityTypeManagerInterface $entity_type_manager,
+    MediaLibraryUiBuilder $library_ui_builder,
+    OpenerResolverInterface $opener_resolver,
+    protected BunnyStreamManagerFactoryInterface $bunnyStreamManagerFactory
+  ) {
+    parent::__construct($entity_type_manager, $library_ui_builder, $opener_resolver);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('media_library.ui_builder'),
+      $container->get('media_library.opener_resolver'),
+      $container->get('bunny_stream.manager')
+    );
+  }
+
 
   /**
    * {@inheritdoc}
@@ -40,6 +77,10 @@ class BunnyStreamMediaLibraryForm extends AddFormBase {
    */
   protected function buildInputElement(array $form, FormStateInterface $form_state) {
 
+    // This is just to validate that we have the correct media type
+    // because getMediaType method throws exception if is not valid.
+    $this->getMediaType($form_state);
+
     // Add a container to group the input elements for styling purposes.
     $form['container'] = [
       '#type' => 'container',
@@ -58,6 +99,7 @@ class BunnyStreamMediaLibraryForm extends AddFormBase {
       '#type' => 'submit',
       '#value' => $this->t('Add'),
       '#button_type' => 'primary',
+      '#validate' => ['::validateVideo'],
       '#submit' => ['::addButtonSubmit'],
       // @todo Move validation in https://www.drupal.org/node/2988215
       '#ajax' => [
@@ -76,6 +118,38 @@ class BunnyStreamMediaLibraryForm extends AddFormBase {
       ],
     ];
     return $form;
+  }
+
+  /**
+   * Validates the given video exists.
+   *
+   * @param array $form
+   *   The complete form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current form state.
+   */
+  public function validateVideo(array &$form, FormStateInterface $form_state) {
+    $video_id = $form_state->getValue('id');
+
+    /** @var \Drupal\bunny_stream\Plugin\media\Source\BunnyStream $source */
+    $source = $this->getMediaType($form_state)->getSource();
+
+    /** @var \Drupal\bunny_stream\BunnyStreamLibraryInterface $library */
+    $library = $source->getLibrary();
+
+    /** @var \Drupal\bunny_stream\VideoManager $video_manager */
+    $video_manager = $this->bunnyStreamManagerFactory->getVideoManager($library->id());
+
+    if ($video_id) {
+      try {
+        if (is_null($video_manager->getVideo($video_id))) {
+          $form_state->setErrorByName('id', $this->t('The given ID is not valid video.'));
+        }
+      }
+      catch (\Exception $exception) {
+        $form_state->setErrorByName('id', $exception->getMessage());
+      }
+    }
   }
 
   /**
